ebde7bd4b3f20a912dbd36d05ea354a75f96a054
tak2022 <wingzero2004jp@gmail.com>
2023-03-21 03:33:52 +0900

    a

--- js/engine/game.js
+++ js/engine/game.js
@@ -18,8 +18,11 @@ class Game {
 		this.canvas.width = width || 320;
 		//canvasの縦幅（ゲームの縦幅）を設定。もし縦幅が指定されていなければ320を代入
 		this.canvas.height = height || 320;
+
+        //ゲームに登場する全てのもの（オブジェクト）を入れるための配列
+		this.objs = [];
 	} //constructor() 終了
-    
+
     /**
 	 * startメソッドを呼び出すことで、メインループが開始される
 	 */
@@ -39,7 +42,25 @@ class Game {
 		//左上から、画面のサイズまでを、塗りつぶす
 		ctx.fillRect( 0, 0, this.canvas.width, this.canvas.height );
 
+        //ゲームに登場する全てのもの（オブジェクト）の数だけ繰り返す
+		for ( let i=0; i<this.objs.length; i++ ) {
+			//スプライトやテキストなど、すべてのオブジェクトのupdateメソッドを呼び出す
+			this.objs[i].update( this.canvas );
+		}
+
 		//自分自身（_mainLoop）を呼び出して、ループさせる
 		requestAnimationFrame( this._mainLoop.bind( this ) );
 	} //_mainLoop() 終了
+
+    /**
+	 * オブジェクトをゲームに追加できるようになる、addメソッドを作成
+	 *
+	 * 引数
+	 * obj : 追加したいオブジェクト
+	 */
+	add( obj ) {
+		//this.objs配列の末尾に、objの値を追加
+		this.objs.push( obj );
+	} //add() 終了
+
 }
